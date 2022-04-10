5bae789178c3c509020916e6d460ab30d2900492
ohahiduzzaman1998 <ohahiduzzaman1998@gmail.com>
2022-04-10 12:10:44 +0600

    all f pactice

---
+++ exam-app/routes/web.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\StartBootstrapController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', [StartBootstrapController::class, 'index'])->name('home');
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified'
+])->group(function () {
+    Route::get('/dashboard',[StartBootstrapController::class, 'index'])->name('dashboard');
+});
